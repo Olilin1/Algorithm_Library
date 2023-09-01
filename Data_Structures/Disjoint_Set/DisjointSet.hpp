3d235794d8aff99bc5d5a0442516b37ee53145e1
Oliver Lindgren <oliver.lindgren@outlook.com>
2023-09-01 15:45:57 +0200

    Cleaned up the disjoint set file

--- Data_Structures/Disjoint_Set/DisjointSet.hpp
+++ Data_Structures/Disjoint_Set/DisjointSet.hpp
@@ -11,68 +11,43 @@
 /*
 A class designed to keep track of N different sets of N total elements.
 Has a function to merge two sets, and a function to check if two elements are in the same set.
-TODO: Change to size_t
 */
+
 class DisjointSet {
 private:
     //Used to keep track of what set elements are in, and the size of that set.
-    std::vector<int> comp; 
+    std::vector<size_t> comp; 
 
     //What other element is a in the same set as? A negative number if a represents itself
-    int representative(unsigned int a);  
+    size_t representative(size_t a){
+        if(comp[a] < 0) return a;
+
+        //recursively find and update our representative
+        comp[a] = representative(comp[a]);
+        return comp[a];
+    }  
 
 public:
     //The constructor takes as input the amount of elements that exists.
-    DisjointSet(unsigned int elements);
+    DisjointSet(size_t elements) : comp(elements, -1) {};
 
     //The unionSets function takes two elements as input and merges the sets that they are in.
-    void unionSets(unsigned int a, unsigned int b);
+    void unionSets(size_t a, size_t b){
+        a = representative(a);
+        b = representative(b);
+
+        if(a == b) return;
+
+        if(comp[a] > comp[b]) std::swap(a,b);
+
+        comp[a] += comp[b]; //The size of set containing a has been increased by size of set containing b.
+        comp[b] = a;
+    }
 
     //The query function takes two elements as input and returns true if they are in the same set. Otherwise it returns false.
-    bool query(unsigned int a, unsigned int b);
+    bool query(size_t a, size_t b){
+        return representative(a) == representative(b);
+    }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DisjointSet::DisjointSet(unsigned int elements) : comp(elements, -1) {};
-
-void DisjointSet::unionSets(unsigned int a, unsigned int b){
-    a = representative(a);
-    b = representative(b);
-
-    if(a == b) return;
-
-    if(comp[a] > comp[b]) std::swap(a,b);
-
-    comp[a] += comp[b]; //The size of set containing a has been increased by size of set containing b.
-    comp[b] = a; //All elements that had b as a representative will now have a as a representative.
-}
-
-int DisjointSet::representative(unsigned int a){
-    if(comp[a] < 0) return a;
-
-    //recursively find and update our representative
-    comp[a] = representative(comp[a]);
-    return comp[a];
-}
-
-bool DisjointSet::query(unsigned int a, unsigned int b){
-    return representative(a) == representative(b);
-}
-
-
 
 #endif
